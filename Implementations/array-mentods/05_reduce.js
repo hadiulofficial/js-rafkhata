94b5eea5d91ae257832caca86ea2058f073219a4
Hadiul Islam <hadiulislamofficial@gmail.com>
2023-07-18 06:51:31 +0000

    updated reduce function

--- Implementations/array-mentods/05_reduce.js
+++ Implementations/array-mentods/05_reduce.js
@@ -1,21 +1,21 @@
-function customeReduce (arr, callback, initialValue){
-    let accumulator = initialValue !== undefined ? initialValue : arr[0]
+function customeReduce(arr, callback, initialValue) {
+  let accumulator = initialValue !== undefined ? initialValue : arr[0];
 
-    for(let i = 0; i < arr.length; i++) {
-        if(accumulator==undefined){
-            accumulator = arr[i]
-        } else {
-            accumulator = callback(accumulator, arr[i], arr)
-        }
-    }
+  for (let i = startIndex; i < arr.length; i++) {
+    accumulator = callback(accumulator, arr[i], arr);
+  }
 
-    return accumulator
+  return accumulator;
 }
 
-const nums = [1,2,3,4,5]
+const nums = [1, 2, 3, 4, 5];
 
-const sumWithInitial = customeReduce(nums, (accumulator, currentValue) => {
-    return accumulator + currentValue
-}, 0)
+const sumWithInitial = customeReduce(
+  nums,
+  (accumulator, currentValue) => {
+    return accumulator + currentValue;
+  },
+  0
+);
 
-console.log(sumWithInitial)
+console.log(sumWithInitial);
