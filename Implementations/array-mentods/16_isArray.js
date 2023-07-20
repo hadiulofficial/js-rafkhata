1310205bcadd175f81f2756e082bd3158888593d
Hadiul Islam <hadiulislamofficial@gmail.com>
2023-07-20 10:57:13 +0000

    added isArray method

---
+++ Implementations/array-mentods/16_isArray.js
@@ -0,0 +1,13 @@
+/*
+The Array.isArray() static method determines whether the passed value is an Array.
+*/
+
+
+function isArray(value){
+    return Object.prototype.toString.call(value) === "[object Array]"
+}
+
+console.log(isArray([3,5]))
+console.log(isArray([]))
+console.log(isArray({}))
+console.log(isArray("string"))
