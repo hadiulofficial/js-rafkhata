c3cd18cd8ea9ba41620777c4e091cb8dea9ec38e
Hadiul Islam <hadiulislamofficial@gmail.com>
2023-07-20 07:21:57 +0000

    feat: added values method

---
+++ Implementations/array-mentods/09_reverse.js
@@ -0,0 +1,6 @@
+/*
+The reverse() method reverses an array in place and returns the reference to the same array, 
+the first array element now becoming the last, and the last array element becoming the first. 
+In other words, elements order in the array will be turned towards the direction opposite to that previously stated.
+*/
+
---
+++ Implementations/array-mentods/10_values.js
@@ -0,0 +1,18 @@
+/*
+The values() method returns a new array iterator object that iterates the value of each item in the array.
+*/
+
+function values(arr) {
+    const values = [];
+  
+    for (let i = 0; i < arr.length; i++) {
+        values.push(arr[i]);
+    }
+  
+    return values;
+  }
+  
+  const words = ['a', 'b', 'c'];
+  const iterator = values(words)
+  
+  console.log(iterator)
